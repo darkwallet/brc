003447603debf8052289b373a471b905556750f1
Amir Taaki <genjix@riseup.net>
2015-01-10 04:24:00 +0100

    correct listen.py

--- test/listen.py
+++ test/listen.py
@@ -4,6 +4,10 @@ s = c.socket(zmq.SUB)
 s.connect("tcp://localhost:9110")
 s.setsockopt(zmq.SUBSCRIBE, "")
 while True:
+    # Tx hash
     print s.recv().encode("hex")
+    # Number of success
+    print s.recv().encode("hex")
+    # Number of failure
     print s.recv().encode("hex")
 
